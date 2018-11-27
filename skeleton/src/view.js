d43d5a00c65c183927c55d5c30dd861ab02f22bd
ryan-mapa <ryan.m.mapa@gmail.com>
2018-11-27 07:15:40 -0800

    Remove code from skeleton

--- skeleton/src/view.js
+++ skeleton/src/view.js
@@ -11,47 +11,29 @@ class View {
   bindEvents() {
     // bind a click handler to each #square that calls press()
 
-    this.$el.on("click", "#square", ( event => {
-      const $square = $(event.currentTarget);
-      this.press($square);
-    }));
   }
 
   static flash($square) {
     // class function that removes css classes and then adds them back
 
-    let color = $square.data("color");
-    $square.removeClass();
-    $square.addClass(`flash`);
     setTimeout(() => {
-      $square.removeClass();
-      $square.addClass(`${color}`);
+
     }, 500);
   }
 
   press($square) {
     // calls flash() and adds to guesses and calls geame's check()
 
-    const color = $square.data("color");
-    View.flash($square);
-    this.game.guesses.push(color);
-      this.game.check();
+
   }
 
   setupBoard() {
     // populates the root element with a $board containing squares
-    
-    const $board = $("<div>");
-    $board.addClass("black");
-    const colors = ['green', 'red', 'yellow', 'blue']
+    // add .black class to $board
+    // add data for color for each square
 
-    for (var i = 0; i < colors.length; i++) {
-      let $square = $("<div>");
-      $square.attr("id", "square");
-      $square.data("color", colors[i]);
-      $square.addClass(`${colors[i]}`);
-      $board.append($square);
-    }
+    const $board = $("<div>");
+
 
     this.$el.append($board);
   }
